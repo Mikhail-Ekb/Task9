0815267112abba27b7f5a51a30d2ef5030704070
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-16 23:10:07 +0500

    Добавьте файлы проекта.

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            foreach(char c in str)
+            {
+                str2 = str + str2;
+            }
+            Console.WriteLine(str2);
+            Console.ReadLKey();
+        }
+    }
+}
